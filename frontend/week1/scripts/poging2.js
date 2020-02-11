86cdf515f627e74f698b1a9e63fe3d1b1f2788aa
KarinMeijvogel <57796369+KarinMeijvogel@users.noreply.github.com>
2020-02-11 15:41:25 +0100

    Update poging2.js

--- frontend/week1/scripts/poging2.js
+++ frontend/week1/scripts/poging2.js
@@ -1,22 +1,21 @@
 function byTagName(node, tagName) {
-let wantedTag = tagName.toUpperCase();
-
-var c = node.children;
-var x = [] ;
-for(var i =0; i<c.length;i++)
- {
+  let wantedTag = tagName.toUpperCase();
+  let c = node.getElementsByTagName("*");
+  let x = [] ;
   
-  if(c[i].tagName===wantedTag){ // children_tag_name should be in capital, eg. "SPAN"
-   x.push(c[i]);
+  for(let i = 0; i < c.length; i++) {
+    if(c[i].tagName===wantedTag){ // add only nodes with the wanted tags to the array
+      x.push(c[i]);
+    }
   }
+  
+  return x
 }
-    return x
-  }
-
-  console.log(byTagName(document.body, "h1").length);
-  // → 1
-  console.log(byTagName(document.body, "span").length);
-  // → 3
-  let para = document.querySelector("p");
-  console.log(byTagName(para, "span").length);
-  // → 2
+  
+console.log(byTagName(document.body, "h1").length);
+// → 1
+console.log(byTagName(document.body, "span").length);
+// → 3
+let para = document.querySelector("p");
+console.log(byTagName(para, "span").length);
+// → 2
