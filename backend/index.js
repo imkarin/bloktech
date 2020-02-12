3da58fccdfa1afaf4dd87424397285702be67170
KarinMeijvogel <karinmeijvogel@hotmail.com>
2020-02-12 22:07:05 +0100

    express test

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,30 @@
 const camelCase = require('camelcase');
-
 console.log(camelCase('hey-there'));
+
+
+// Express
+const express = require('express');
+
+// Init express
+const app = express();
+
+// Listen on a port
+app.listen(3000);
+
+// Creating end points/route handlers
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About route')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Contact route')
+})
+
+// 404 - route not found
+app.get('/*', (req, res) => {
+    res.send('oops')
+})
