c3496efba4c5a23088d78b04fc1c15962b8d3038
Karin Meijvogel <karinmeijvogel@hotmail.com>
2020-02-27 16:36:32 +0100

    First EJS test

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ console.log(camelCase('hey-there'));
 // Require node's path module
 const path = require('path');
 
-// Express
+// Express ---------------------------------------------------------------------------------------
 const express = require('express');
 
 // Init express
@@ -14,17 +14,17 @@ const app = express();
 // Define static files folder
 app.use(express.static('public'));
 
-// Creating end points/route handlers
+
+// EJS -------------------------------------------------------------------------------------------
+app.set('view engine', 'ejs');
+
+// Creating end points/route handlers ------------------------------------------------------------
 app.get('/', (req, res) => {
     res.send('Welcome to the homepage');
 })
 
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/pagina.html'));
-})
-
-app.get('/contact', (req, res) => {
-    res.send('Contact route');
 })
 
 app.get('/mp3', (req, res) => {
@@ -35,6 +35,15 @@ app.get('/profile', (req, res) => {
   console.log(req.query);
   res.send(`You're viewing ` + req.query['username'] + `'s profile`);
 })
+
+app.get('/user/:username', function (req, res) {
+    
+// Objects
+    const data = {naam: req.params['username'], movies: ['Uncut Gems', 'Inception', 'Alita: Battle Angel']};
+    res.render('index', data)
+  })
+
+  
 
 
 // 404 - route not found
