913ab6e3008bc42fed92c9e59c2e9b619e1b40ed
Karin Meijvogel <karinmeijvogel@hotmail.com>
2020-03-02 14:04:25 +0100

    Like button press is being registered to server

--- index.js
+++ index.js
@@ -17,10 +17,36 @@ app.use('/static', express.static('static'));
 // EJS -------------------------------------------------------------------------------------------
 app.set('view engine', 'ejs');
 
+// Use body-parser and slug ----------------------------------------------------------------------
+const slug = require('slug');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+// users and their data
+class Person{
+  constructor(name, age, id, photo, msg) {
+      this.name = name;
+      this.age = age;
+      this.id = id;
+      this.photo = photo;
+      this.msg = msg;
+  }
+}
+
+let Olivia = new Person('Olivia Delroy', '24', '001', 'girl.jpeg', `Hey! I love all of the bands in your...`)
+let Kayla = new Person('Kayla Solomon', '25', '002', 'girl1.jpeg', `What's up :-)`)
+let Nadia = new Person('Nadia Williams', '23', '003', 'girl2.jpeg', `lol ikr`)
+let Eve = new Person('Eve Johnson', '24', '004', 'girl3.jpeg', `Are you going to DLDK this year?`)
+let Abby = new Person('Abby Watts', '25', '005', 'girl4.jpeg', `Yeah, saw them live 3 weeks ago.`)
+let Christina = new Person('Christina May', '25', '006', 'girl5.jpeg', `Digimon or pokemon?`)
+
+const data = {
+  likedPeople: [Olivia, Kayla, Nadia, Eve, Abby, Christina]
+}
+
 // Creating end points/route handlers ------------------------------------------------------------
 
-/* Old homework
-
+/*   Old homework
 // Send static (html) file:
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname + '/static/pagina.html'));
@@ -35,9 +61,7 @@ app.get('/mp3', (req, res) => {
 app.get('/user/:username', function (req, res) {
   const data = {naam: req.params['username'], movies: ['Uncut Gems', 'Inception', 'Alita: Battle Angel']};
   res.render('pagina', data)
-})
-
-*/
+})   */
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -52,30 +76,26 @@ app.get('/profile', (req, res) => {
 })
 
 app.get('/likedpage', (req, res) => {
-  // users
-  class Person{
-    constructor(name, age, id, photo, msg) {
-        this.name = name;
-        this.age = age;
-        this.id = id;
-        this.photo = photo;
-        this.msg = msg;
-    }
-  }
 
-  let Olivia = new Person('Olivia Delroy', '24', '001', 'girl.jpeg', `Hey! I love all of the bands in your...`)
-  let Kayla = new Person('Kayla Solomon', '25', '002', 'girl1.jpeg', `What's up :-)`)
-  let Nadia = new Person('Nadia Williams', '23', '003', 'girl2.jpeg', `lol ikr`)
-  let Eve = new Person('Eve Johnson', '24', '004', 'girl3.jpeg', `Are you going to DLDK this year?`)
-  let Abby = new Person('Abby Watts', '25', '005', 'girl4.jpeg', `Yeah, saw them live 3 weeks ago.`)
-  let Christina = new Person('Christina May', '25', '006', 'girl5.jpeg', `Digimon or pokemon?`)
-
-  const data = {
-    likedPeople: [Olivia, Kayla, Nadia, Eve, Abby, Christina]
-  }
 
   res.render('likedpage', data);
 })
+
+// Get input from form on the /add page (week 3 BE class)
+app.get('/add', (req, res) => {
+  res.render('add'); 
+})
+
+app.post('/', (req, res) => { // when the user 'submits' the form, the title gets console logged
+  if (req.body['liked']) {
+    console.log('You liked this person!');
+  }
+
+  else {
+    console.log('You did not like this person');
+  }
+})
+
 
 
 // Listen on a port
